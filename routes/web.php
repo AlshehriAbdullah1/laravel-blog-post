6f7d2795dc2ecf695c7ac0c6de653794040e62b2
abdullah <stay__away121@hotmail.com>
2023-07-11 16:28:34 +0300

    adding filters,

---
+++ routes/web.php
@@ -0,0 +1,60 @@
+<?php
+
+use App\Models\Post;
+use App\Models\Category;
+use App\Models\User;
+use Illuminate\Support\Facades\Route;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+   return view('posts',[
+    'posts'=>Post::latest()->get(),
+    'categories'=>Category::all(),
+   ]);
+    
+});
+
+
+Route::get('posts/{post:slug}', function(Post $post){
+
+   
+
+
+
+    return view('post',[
+        'post'=>$post,
+        
+
+    ]);
+
+});
+
+
+Route::get('categories/{category:slug}',function(Category $category){
+    //['items'][0]['attributes']
+    // dd($category->posts);
+    return view('posts',[
+        'posts' => $category->posts,
+        'currentCategory'=>$category,
+        'categories'=>Category::all(),
+    ]);
+});
+Route::get('authors/{author:username}',function(User $author){
+    //['items'][0]['attributes']
+     //dd($author);
+    return view('posts',[
+        'posts' => $author->posts,
+        'categories'=>Category::all(),
+    ]);
+});
